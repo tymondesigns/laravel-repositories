e3de1686b75b13a1a033ceddf327e59b03b77cd3
Sean Tymon <tymon148@gmail.com>
2014-12-30 11:36:33 +0000

    adding columns parameter

--- src/Eloquent/AbstractRepository.php
+++ src/Eloquent/AbstractRepository.php
@@ -5,11 +5,15 @@ namespace Tymon\Repositories\Eloquent;
 abstract class AbstractRepository {
 
     /**
+     * The model instance
+     * 
      * @var Illuminate\Database\Eloquent\Model
      */
     protected $model;
 
     /**
+     * The model relations
+     * 
      * @var array
      */
     protected $with = [];
@@ -38,34 +42,36 @@ abstract class AbstractRepository {
     /**
      * Retrieve all entities
      *
+     * @param  array  $columns
      * @return Illuminate\Database\Eloquent\Collection
      */
-    public function all()
+    public function all(array $columns = ['*'])
     {
-        return $this->make()->get();
+        return $this->make()->get($columns);
     }
 
     /**
      * Find a single entity
      *
-     * @param  int  $id
+     * @param  int    $id
+     * @param  array  $columns
      * @return Illuminate\Database\Eloquent\Model
      */
-    public function find($id)
+    public function find($id, array $columns = ['*'])
     {
-        return $this->make()->find($id);
+        return $this->make()->find($id, $columns);
     }
 
     /**
     * Get Results by Page
     *
-    * @param  int    $page
     * @param  int    $limit
+    * @param  array  $columns
     * @return \Illuminate\Pagination\Paginator
     */
-    public function getByPage($limit = 10)
+    public function getByPage($limit = 10, array $columns = ['*'])
     {
-        return $this->make()->paginate($limit);
+        return $this->make()->paginate($limit, $columns);
     }
 
     /**
@@ -73,11 +79,12 @@ abstract class AbstractRepository {
      *
      * @param  string  $key
      * @param  mixed   $value
+     * @param  array   $columns
      * @return Illuminate\Database\Query\Builder
      */
-    public function getManyBy($key, $value)
+    public function getManyBy($key, $value, array $columns = ['*'])
     {
-        return $this->make()->where($key, $value)->get();
+        return $this->make()->where($key, $value)->get($columns);
     }
 
     /**
@@ -85,11 +92,12 @@ abstract class AbstractRepository {
      *
      * @param  string  $key
      * @param  mixed   $value
+     * @param  array   $columns
      * @return Illuminate\Database\Query\Builder
      */
-    public function getFirstBy($key, $value)
+    public function getFirstBy($key, $value, array $columns = ['*'])
     {
-        return $this->make()->where($key, $value)->first();
+        return $this->make()->where($key, $value)->first($columns);
     }
 
     /**
@@ -97,11 +105,12 @@ abstract class AbstractRepository {
      *
      * @param  string  $key
      * @param  array   $array
+     * @param  array   $columns
      * @return Illuminate\Database\Query\Builders
      */
-    public function getWhereIn($key, array $array)
+    public function getWhereIn($key, array $array, array $columns = ['*'])
     {
-        return $this->make()->whereIn($key, $array)->get();
+        return $this->make()->whereIn($key, $array)->get($columns);
     }
 
 }
