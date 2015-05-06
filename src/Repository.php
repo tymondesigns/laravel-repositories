860e2264e8088dcda2b9bede852eed53327d3321
Pulkit Jalan <pulkitjalan@users.noreply.github.com>
2015-05-06 14:58:13 +0100

    Fix brackets

--- src/Repository.php
+++ src/Repository.php
@@ -202,7 +202,7 @@ abstract class Repository implements RepositoryContract {
             return call_user_func_array([$this->model, $method], $parameters);
         }
 
-        throw new \BadMethodCallException(sprintf('Method [%s] does not exist.', $method);
+        throw new \BadMethodCallException(sprintf('Method [%s] does not exist.', $method));
     }
 
 }
